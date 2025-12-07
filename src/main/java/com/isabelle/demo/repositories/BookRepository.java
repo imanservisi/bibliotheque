a6e93ae8c7a4bda546798c14da955ea664a3a635
imanservisi <popovicsisabelle@gmail.com>
2025-12-07 11:36:27 +0100

    suppression rest

--- src/main/java/com/isabelle/demo/repositories/BookRepository.java
+++ src/main/java/com/isabelle/demo/repositories/BookRepository.java
@@ -3,11 +3,9 @@ package com.isabelle.demo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.isabelle.demo.entities.Book;
 
-@RepositoryRestResource(path= "rest")
 public interface BookRepository extends JpaRepository<Book, Long>{
     List<Book> findAllByOrderByNameAsc();
     List<Book> findByName(String name);
